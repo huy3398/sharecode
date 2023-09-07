8c69a5b104d4b9cda2cbfbcd534b10b1290a9721
Huy Kimochi <getemnhat@gmail.com>
2023-09-07 18:24:25 +0700

    demotool

---
+++ demo/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePickerStart1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerStart2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerStart = new System.Windows.Forms.TextBox();
+            this.dateTimePickerEnd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePickerStart1
+            // 
+            this.dateTimePickerStart1.Location = new System.Drawing.Point(41, 22);
+            this.dateTimePickerStart1.Name = "dateTimePickerStart1";
+            this.dateTimePickerStart1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerStart1.TabIndex = 1;
+            this.dateTimePickerStart1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePickerStart2
+            // 
+            this.dateTimePickerStart2.Location = new System.Drawing.Point(41, 66);
+            this.dateTimePickerStart2.Name = "dateTimePickerStart2";
+            this.dateTimePickerStart2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerStart2.TabIndex = 2;
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.Location = new System.Drawing.Point(223, 163);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(218, 20);
+            this.dateTimePickerStart.TabIndex = 3;
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(223, 189);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(218, 20);
+            this.dateTimePickerEnd.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePickerEnd);
+            this.Controls.Add(this.dateTimePickerStart);
+            this.Controls.Add(this.dateTimePickerStart2);
+            this.Controls.Add(this.dateTimePickerStart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart2;
+        private System.Windows.Forms.TextBox dateTimePickerStart;
+        private System.Windows.Forms.TextBox dateTimePickerEnd;
+    }
+}
+
---
+++ demo/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Chuỗi kết nối đến SQL Server
+          
+           string connectionString1 = @"Data Source=localhost;Initial Catalog=ami_lt;Integrated Security=True";
+            //  string connectionString2 = "Server=192.168.1.21;Database=ami_lt;User Id=ami;Password=protnc;";
+
+            // Thực hiện kết nối và truy vấn
+            string query = "SELECT judge, priority_defect_name, panelid, display_ins, start_time FROM product WHERE start_time >= @StartTime AND start_time <= @EndTime";
+            using (SqlConnection connection1 = new SqlConnection(connectionString1))
+          //  using (SqlConnection connection2 = new SqlConnection(connectionString2))
+            {
+                connection1.Open();
+               // connection2.Open();
+
+                SqlCommand command1 = new SqlCommand(query, connection1);
+              //  SqlCommand command2 = new SqlCommand(query, connection2);
+
+                // Đặt giá trị thời gian bắt đầu và kết thúc từ TextBox trên giao diện
+                command1.Parameters.AddWithValue("@StartTime", dateTimePickerStart.Text);
+                //command1.Parameters.AddWithValue("@StartTime", TextBox.text);
+                command1.Parameters.AddWithValue("@EndTime", dateTimePickerEnd.Text);
+
+               // command2.Parameters.AddWithValue("@StartTime", dateTimePickerStart.Text);
+              //  command2.Parameters.AddWithValue("@EndTime", dateTimePickerEnd.Text);
+
+                // Thực hiện truy vấn và ghi dữ liệu vào tệp tin
+                using (StreamWriter sw = new StreamWriter("output.txt"))
+                {
+                    SqlDataReader reader1 = command1.ExecuteReader();
+                    //SqlDataReader reader2 = command2.ExecuteReader();
+
+                    while (reader1.Read())
+                    {
+                        sw.WriteLine("192.168.1.20 = *401: " + reader1["judge"] + ", " + reader1["priority_defect_name"] + ", " + reader1["panelid"] + ", " + reader1["display_ins"] + ", " + reader1["start_time"]);
+                    }
+
+                   // while (reader2.Read())
+                  //  {
+                      //  sw.WriteLine("192.168.1.20 = *402: " + reader2["judge"] + ", " + reader2["priority_defect_name"] + ", " + reader2["panelid"] + ", " + reader2["display_ins"] + ", " + reader2["start_time"]);
+                  //  }
+                
+                    reader1.Close();
+                 //   reader2.Close();
+                }
+            }
+
+            MessageBox.Show("Xong! Dữ liệu đã được xuất ra file output.txt.");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
